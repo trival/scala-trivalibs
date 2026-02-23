fe99a0b1d0bbb502778ed20916f8d8ef32b03d25
Thomas Gorny <thomas.gorny@pm.me>
2026-02-23 14:02:09 +0100

    cleanup number type class

--- src/utils/numbers.scala
+++ src/utils/numbers.scala
@@ -1,42 +1,31 @@
 package trivalibs.utils.numbers
 
-trait Number[P]:
-  given numeric: Numeric[P]
+trait NumExt[P]:
   extension (p: P)
-    inline def clamp(min: P, max: P): P =
-      if numeric.lt(p, min) then min
-      else if numeric.gt(p, max) then max
-      else p
-    inline def clamp01: P = p.clamp(numeric.zero, numeric.one)
-
-object Number:
-  given Number[Float]:
-    val numeric = summon[Numeric[Float]]
-  given Number[Double]:
-    val numeric = summon[Numeric[Double]]
-
-  given [P](using n: Number[P]): Numeric[P] = n.numeric
-
-trait Floating[P] extends Number[P]:
-  extension (p: P)
-    def sqrt: Double
-    def pow(exp: P): Double
+    def clamp(min: P, max: P): P
+    def clamp01: P
+    def sqrt: P
+    def pow(exp: P): P
     def fit0111: P
     def fit1101: P
 
-object Floating:
-  given Floating[Double]:
-    val numeric = summon[Numeric[Double]]
+object NumExt:
+  given NumExt[Double]:
     extension (p: Double)
-      inline def sqrt         = Math.sqrt(p)
+      inline def clamp(min: Double, max: Double) =
+        if p < min then min else if p > max then max else p
+      inline def clamp01 = clamp(0.0, 1.0)
+      inline def sqrt = Math.sqrt(p)
       inline def pow(e: Double) = Math.pow(p, e)
-      inline def fit0111      = p * 2.0 - 1.0
-      inline def fit1101      = p * 0.5 + 0.5
+      inline def fit0111 = p * 2.0 - 1.0
+      inline def fit1101 = p * 0.5 + 0.5
 
-  given Floating[Float]:
-    val numeric = summon[Numeric[Float]]
+  given NumExt[Float]:
     extension (p: Float)
-      inline def sqrt         = Math.sqrt(p)
-      inline def pow(e: Float)  = Math.pow(p, e)
-      inline def fit0111      = (p * 2.0 - 1.0).toFloat
-      inline def fit1101      = (p * 0.5 + 0.5).toFloat
+      inline def clamp(min: Float, max: Float) =
+        if p < min then min else if p > max then max else p
+      inline def clamp01 = clamp(0f, 1f)
+      inline def sqrt = Math.sqrt(p).toFloat
+      inline def pow(e: Float) = Math.pow(p, e).toFloat
+      inline def fit0111 = (p * 2.0 - 1.0).toFloat
+      inline def fit1101 = (p * 0.5 + 0.5).toFloat
