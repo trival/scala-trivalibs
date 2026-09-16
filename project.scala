0b47bc84a425ceb6fbeaccf47962dcd593a998f7
Thomas Gorny <thomas.gorny@pm.me>
2026-09-16 11:54:38 +0200

    update libs

--- project.scala
+++ project.scala
@@ -1,10 +1,10 @@
-//> using scala 3.8.4
+//> using scala 3.9.0
 //> using platform js
 //> using option -Wconf:msg=differs.only.in.case:s
 //> using option -feature
 //> using option -language:implicitConversions
 
-//> using jsVersion 1.21.0
+//> using jsVersion 1.22.0
 //> using jsMode full
 //> using jsModuleKind es
 //> using jsEsVersionStr es2021
@@ -12,4 +12,4 @@
 
 //> using dep org.scala-js::scalajs-dom::2.8.1
 
-//> using test.dep org.scalameta::munit::1.3.3
+//> using test.dep org.scalameta::munit::1.3.6
